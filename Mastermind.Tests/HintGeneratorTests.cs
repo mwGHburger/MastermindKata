93c2de8a5a05633547b30572c2f5e7718ad490a5
Michael Wang <michael.wang@myob.com>
2020-12-09 17:55:25 +1100

    refactored

--- Mastermind.Tests/HintGeneratorTests.cs
+++ Mastermind.Tests/HintGeneratorTests.cs
@@ -35,7 +35,13 @@ namespace Mastermind.Tests
         public void Generate_ShouldReturn_AShuffledHintList_ForCorrectColours_InTheCorrectPosition()
         {
             var mockListShuffler = new Mock<IListShuffler>();
-            var validColours = TestHelper.SetupWinningColours();
+            var validColours = new List<string>()
+            {
+                "Red",
+                "Green",
+                "Blue",
+                "Green"
+            };
             var playerInput = new List<string>()
             {
                 "Green", "Green", "Green", "Green"
@@ -44,10 +50,12 @@ namespace Mastermind.Tests
             var expected = new List<string>()
             {
                 "Black",
+                "Black",
             };
 
-            mockListShuffler.Setup(x => x.Shuffle(It.Is<List<string>>(x => x.FindAll(i => i.Equals("Black")).Count.Equals(1)))).Returns(new List<string>()
+            mockListShuffler.Setup(x => x.Shuffle(It.Is<List<string>>(x => x.FindAll(i => i.Equals("Black")).Count.Equals(2)))).Returns(new List<string>()
             {
+                "Black",
                 "Black",
             });
 
@@ -71,7 +79,14 @@ namespace Mastermind.Tests
                 "White", "Black"
             };
 
-            mockListShuffler.Setup(x => x.Shuffle(It.IsAny<List<string>>())).Returns(new List<string>()
+            mockListShuffler.Setup(x => x.Shuffle(
+                It.Is<List<string>>(
+                    x => x.FindAll(
+                        i => i.Equals("Black") || i.Equals("White")
+                    ).Count.Equals(2)
+                )
+                ))
+            .Returns(new List<string>()
             {
                 "White",
                 "Black"
--- Mastermind/HintGenerator/HintGenerator.cs
+++ Mastermind/HintGenerator/HintGenerator.cs
@@ -14,41 +14,42 @@ namespace Mastermind
         }
         public List<string> Generate(List<string> inputColours, List<string> winningColours)
         {
+            List<string> winningColoursCopy = new List<string>(winningColours);
             var encryptedColours = new List<string>();
             for(int i = 0; i < _numberOfWinningColours; i++)
             {
-                if(inputColours[i].Equals(winningColours[i]))
+                if(inputColours[i].Equals(winningColoursCopy[i]))
                 {
-                    HandleCorrectPosition(encryptedColours, winningColours, i);
+                    HandleCorrectPosition(encryptedColours, winningColoursCopy, i);
                 }
-                else if(winningColours.Contains(inputColours[i]))
+                else if(winningColoursCopy.Contains(inputColours[i]))
                 {
                     var correctColour = inputColours[i];
-                    var correctIndex = winningColours.FindIndex(x => x.Equals(correctColour));
-                    if(inputColours[correctIndex].Equals(winningColours[correctIndex]))
+                    var correctIndex = winningColoursCopy.FindIndex(x => x.Equals(correctColour));
+                    if(inputColours[correctIndex].Equals(winningColoursCopy[correctIndex]))
                     {
-                        HandleCorrectPosition(encryptedColours, winningColours, correctIndex);
+                        HandleCorrectPosition(encryptedColours, winningColoursCopy, correctIndex);
                     }
                     else
                     {
-                        HandleIncorrectPosition(encryptedColours, winningColours, correctColour);
+                        HandleIncorrectPosition(encryptedColours, winningColoursCopy, correctColour);
                     }
                 }
             }
             return _listShuffler.Shuffle(encryptedColours);
         }
 
-        private void HandleCorrectPosition(List<string> encryptedColours, List<string> winningColours, int index)
+        private void HandleCorrectPosition(List<string> encryptedColours, List<string> winningColoursCopy, int index)
         {
             encryptedColours.Add(StandardMessages.CorrectIndexPosition());
-            winningColours[index] = "null";
+            winningColoursCopy[index] = "null";
         }
 
-        private void HandleIncorrectPosition(List<string> encryptedColours, List<string> winningColours, string correctColour)
+        private void HandleIncorrectPosition(List<string> encryptedColours, List<string> winningColoursCopy, string correctColour)
         {
             encryptedColours.Add(StandardMessages.WrongIndexPosition());
-            var index = winningColours.FindIndex(x => x.Equals(correctColour));
-            winningColours[index] = "null";
+            var index = winningColoursCopy.FindIndex(x => x.Equals(correctColour));
+            winningColoursCopy[index] = "null";
         }
     }
 }
